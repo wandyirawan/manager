e1d164567fcb156f18f1f65fcc01840c01916212
Cecep Wandy Irawan <UCXI003@marketing-asiapac.com>
2018-02-19 17:28:50 +0700

    lecture 148

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,6 +1,7 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { FlatList,View, Text } from 'react-native';
 import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
@@ -8,6 +9,7 @@ class EmployeeList extends Component {
     this.props.employeesFetch();
   }
   render() {
+    console.log('a',this.props);
     return (
       <View>
         <Text>Employee list</Text>
@@ -20,5 +22,13 @@ class EmployeeList extends Component {
     );
   }
 }
+const mapStateToProps = state => {
+  console.log(state.employees,'this sparta');
+  const employees = _.map(state.employees,(val,uid)=>{
+    return {...val,uid};
+  });
 
-export default connect(null,{employeesFetch})(EmployeeList);
+  return {employees};
+};
+
+export default connect(mapStateToProps,{employeesFetch})(EmployeeList);
